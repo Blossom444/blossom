70106fcdd2d0a2bedcf88da4cd54d271db4a6023
Blossom444 <xxxxxwolf96@icloud.com>
2025-05-23 18:24:56 +0200

    Оновлено конфігурацію Next.js для кращої підтримки зображень

--- next.config.js
+++ next.config.js
@@ -2,29 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp3|wav|ico|png|jpg|jpeg|gif|svg)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'static/media/',
-          publicPath: '/static/media/',
-        },
-      },
-    });
-    return config;
-  },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/static/**',
-      },
-    ],
+    domains: ['localhost'],
+    unoptimized: true,
   },
   // Security and optimization
   poweredByHeader: false,
